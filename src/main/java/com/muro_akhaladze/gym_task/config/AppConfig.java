f387943cc236caf09c1470b6e256597d0bf1890f
Muro <makhaladze13@gmail.com>
2025-03-06 23:37:14 +0400

    created and updated config

--- src/main/java/com/muro_akhaladze/gym_task/config/AppConfig.java
+++ src/main/java/com/muro_akhaladze/gym_task/config/AppConfig.java
@@ -1,11 +1,17 @@
 package com.muro_akhaladze.gym_task.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @Configuration
 @ComponentScan(basePackages = "com.muro_akhaladze.gym_task")
 @PropertySource("classpath:application.properties")
 public class AppConfig {
+    @Bean
+    public LocalValidatorFactoryBean validator() {
+        return new LocalValidatorFactoryBean();
+    }
 }
